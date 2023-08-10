2505def8159288fea321fac819aed945a5159000
Gregor <gregor-peters@online.de>
2023-08-10 11:45:41 +0200

    Add jwt generation to server

--- gain-graph-server/src/server.ts
+++ gain-graph-server/src/server.ts
@@ -1,7 +1,35 @@
 import express, { Express, Request, Response } from 'express';
-
+import cors from 'cors';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import {createHash } from 'crypto';
+import bodyParser from 'body-parser';
+const jsonParser = bodyParser.json();
 const app: Express = express();
 const port = 3001;
+dotenv.config();
+app.use(cors())
+
+function generateAccessToken(username: string) {
+    return jwt.sign({ name: username }, process.env.TOKEN_SECRET as jwt.Secret, { expiresIn: 3600 } as jwt.SignOptions);
+}
+
+app.post('/login', jsonParser, (req: Request, res: Response) => {
+    var { username, password } = req.body;
+    password+="salt"
+    password = createHash('sha256').update(password).digest('base64');
+    switch (username) {
+        case "Gregor":
+            if (password == 'TUaPygL5Q0uVMiMavwNRNp4AS49EdT9d3xmXKXtjw/Y=') break;
+        default:
+            res.status(403);
+            return res.send("wrong password");
+        
+    }
+    res.send({
+        token: generateAccessToken(username)
+    })
+})
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
