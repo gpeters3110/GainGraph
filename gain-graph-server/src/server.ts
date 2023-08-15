3e3090cc8bb7220e8fff0056cbce992a60936432
Gregor <gregor-peters@online.de>
2023-08-15 22:53:22 +0200

    jwt on all endpoints except login

--- gain-graph-server/src/server.ts
+++ gain-graph-server/src/server.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
@@ -11,10 +11,37 @@ dotenv.config();
 app.use(cors())
 
 function generateAccessToken(username: string) {
-    return jwt.sign({ name: username }, process.env.TOKEN_SECRET as jwt.Secret, { expiresIn: 3600 } as jwt.SignOptions);
+    return jwt.sign({ name: username }, process.env.TOKEN_SECRET as jwt.Secret, { expiresIn: 10 } as jwt.SignOptions);
 }
-
-app.post('/login', jsonParser, (req: Request, res: Response) => {
+app.use((req: Request, res: Response, next: NextFunction) => { 
+    console.log('checking Token');
+    console.log(req.path);
+    console.log(req.method);
+    if (req.path == '/user' && req.method == 'POST') {
+        console.log('login attempt, skip.');
+        next();
+    }
+    else {
+        const token = req.headers.authorization;
+        if (!token) {
+            res.status(401);
+            console.log('token invalid!');
+            return res.send('Invalid Token');
+        }
+        console.log('token encode: ' + JSON.stringify(token));
+        console.log("token decode: "+ JSON.stringify(jwt.decode(token)));
+        try {
+            jwt.verify(token, process.env.TOKEN_SECRET as jwt.Secret);
+            console.log("token valid!")
+            next();
+        } catch (err) {
+            res.status(401);
+            console.log('token invalid!');
+            return res.send('Invalid Token');
+        }
+    }
+});
+app.post('/user', jsonParser, (req: Request, res: Response) => {
     var { username, password } = req.body;
     password+="salt"
     password = createHash('sha256').update(password).digest('base64');
@@ -26,11 +53,16 @@ app.post('/login', jsonParser, (req: Request, res: Response) => {
             return res.send("wrong password");
         
     }
-    res.send({
-        token: generateAccessToken(username)
-    })
+    res.send({token: generateAccessToken(username) })
 })
-
+app.get('/user', (req, res) => {
+    console.log('validating');
+    const token = req.headers.authorization;
+    if (token) {
+        console.log(jwt.decode(token));
+        res.send(jwt.decode(token))
+    }
+})
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
